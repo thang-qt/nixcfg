6f52c1750dbc188373d0c10d921afe70f09e15da
Quang Thang <thang@thangqt.com>
2026-01-17 23:31:04 +0700

    enable fcitx5 on pathway

--- nixos/pathway/configuration.nix
+++ nixos/pathway/configuration.nix
@@ -30,6 +30,17 @@
     variant = "";
   };
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      libsForQt5.fcitx5-qt
+      qt6Packages.fcitx5-configtool
+      fcitx5-bamboo
+    ];
+  };
+
   services.printing.enable = true;
 
   services.power-profiles-daemon.enable = false;
