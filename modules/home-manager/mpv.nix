b658b5e4d2a5bcd94fd645688df8b7d6b3650e42
Quang Thang <thang@thangqt.com>
2026-01-25 20:27:43 +0700

    enhance mpv config

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -22,6 +22,15 @@
     config = {
       profile = "high-quality";
       ytdl-format = "bestvideo+bestaudio";
+      slang = "vie,vi,eng,en";
+      cache = "yes";
+      demuxer-max-bytes = "4G";
+      cache-backbuffer = "yes";
+      demuxer-max-back-bytes = "1G";
+      keep-open = "yes";
+      keep-open-pause = "yes";
+      save-position-on-quit = "yes";
+      sub-auto = "fuzzy";
     };
   };
 }
