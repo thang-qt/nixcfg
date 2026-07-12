4c6baaea0c997ec66d08b88ef1ec58b2a4fade86
Quang Thang <thang@thangqt.com>
2026-07-12 19:09:42 +0700

    mpv remember history

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -28,6 +28,8 @@
       keep-open = "yes";
       keep-open-pause = "yes";
       save-position-on-quit = "yes";
+      # Keep mpv alive when started without files, so keep-session can restore the last file/session.
+      idle = "yes";
       # When opening one local file, create a playlist from sibling media files so playback advances.
       autocreate-playlist = "same";
       # Required by Trakt Scrobbler for mpv progress/file tracking.
@@ -35,4 +37,18 @@
       sub-auto = "fuzzy";
     };
   };
+
+  # Restore the last played file/session when launching `mpv` without arguments.
+  # Source: https://github.com/CogentRedTester/mpv-scripts/blob/master/keep-session.lua
+  home.file.".config/mpv/scripts/keep-session.lua".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/CogentRedTester/mpv-scripts/master/keep-session.lua";
+    hash = "sha256-kRM5qhU4rZOEnCSxpiO0vaEVjNb/jOPUiCIH9+8mQLc=";
+  };
+
+  home.file.".config/mpv/script-opts/keep_session.conf".text = ''
+    auto_save=yes
+    auto_load=yes
+    load_playlist=no
+    maintain_pos=yes
+  '';
 }
