682acf52a1c37e52dfa619f246428ed315a09155
Quang Thang <thang@thangqt.com>
2026-07-28 21:01:50 +0700

    petri: add terminal development tools

--- home/thang/petri/home.nix
+++ home/thang/petri/home.nix
@@ -5,8 +5,24 @@
   pkgs,
   ...
 }: {
+  nixpkgs.overlays = [
+    inputs.self.overlays.llm-agents
+  ];
+
   imports = [
     inputs.sops-nix.homeManagerModules.sops
     ../common.nix
+    inputs.self.homeManagerModules.pi
+    inputs.self.homeManagerModules.zellij
   ];
+
+  home.packages = with pkgs; [
+    gh
+    uv
+  ];
+
+  programs.pi-coding-agent = {
+    enable = true;
+    extraPackages = [];
+  };
 }
